9f5ef304db3effab75a916a70b1760bc36fe641e
ekato99 <133620407+cookiekingjlim@users.noreply.github.com>
2023-08-10 17:27:43 +0900

    feat: FactoryPattern 적용2

---
+++ 17_FactoryPattern/src/servlet/controller/component/FindController.java
@@ -0,0 +1,32 @@
+package servlet.controller.component;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import servlet.controller.Controller;
+import servlet.model.dao.MemberDAO;
+import servlet.model.vo.MemberVO;
+
+public class FindController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		String path = "find_fail.jsp";
+		String id = request.getParameter("id");
+		MemberVO vo;
+		try {
+			vo = MemberDAO.getInstance().findByIdMember(id);
+			if(vo!=null) {
+				request.setAttribute("vo", vo);
+				path = "find_ok.jsp";
+			}
+		} catch (SQLException e) {}
+		
+		return path;
+		
+	}
+
+	
+}
