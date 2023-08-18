0d83b2a1e5b0ce0b9935292459dc509616dad18e
ekato99 <133620407+cookiekingjlim@users.noreply.github.com>
2023-08-18 17:46:29 +0900

    feat: Servlet

--- 05_ConfigServlet/src/servlet/config/ConfigServlet1.java
+++ 05_ConfigServlet/src/servlet/config/ConfigServlet1.java
@@ -9,23 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ConfigServlet1
- */
 public class ConfigServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private String name, addr;
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
+
 	public void init(ServletConfig config) throws ServletException {
 		name = config.getInitParameter("name");
 		addr = config.getInitParameter("addr");
+		System.out.println(name + ", " + addr);
 	}
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
@@ -36,4 +29,5 @@ public class ConfigServlet1 extends HttpServlet {
 		out.close();
 	}
 
+}
 }
