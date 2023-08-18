b5280297fa1a90b20a6bbad33c905927ae3b769a
ekato99 <133620407+cookiekingjlim@users.noreply.github.com>
2023-08-18 17:47:52 +0900

    feat : MyBatis 이론

---
+++ 22_MyBatis/WebContent/views/update.jsp
@@ -0,0 +1,32 @@
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h2>회원 정보 수정</h2>
+	<hr>
+	
+	<%-- EL 구문 사용으로 필요없어
+	<% MemberDTO dto = (MemberDTO) session.getAttribute("dto");
+	 if(dto != null){
+	%>
+	--%>
+		<form action="/update.do" method="post">
+		<input type="hidden" name="command" value="update">
+		아이디 : <input type="text" name="id" value="${vo.id}" readonly><br>
+		비밀번호 : <input type="password" name="password" value="${vo.password}" required><br>
+		이름 : <input type="text" name="name" value="${vo.name}" required ><br>
+		주소 : <input type="text" name="address" value="${vo.address}" required><br>
+		<input type="submit" value="변경하기">
+		</form>
+		<a href="/index.jsp">첫 페이지로 돌아가기</a>
+
+	<%--<% } --%>
+</body>
+</html>
---
+++ 22_MyBatis/src/member/controller/HandlerMapping.java
@@ -0,0 +1,38 @@
+package member.controller;
+
+import member.controller.component.AllMemberController;
+import member.controller.component.FindController;
+import member.controller.component.LoginController;
+import member.controller.component.LogoutController;
+import member.controller.component.RegisterController;
+import member.controller.component.UpdateController;
+
+public class HandlerMapping {
+	private static HandlerMapping handler = new HandlerMapping();
+	private HandlerMapping() {}
+	public static HandlerMapping getInstance() {
+		return handler;
+	}
+	
+	public Controller createController(String command) {
+		Controller controller = null;
+//		System.out.println("adsads");
+		if(command.equals("find.do")) {
+			controller = new FindController();
+		} else if(command.equals("register.do")) {
+//			System.out.println("adsads");
+			controller = new RegisterController();
+		} else if(command.equals("login.do")) {
+//			System.out.println("adsads");
+			controller = new LoginController();
+		} else if(command.equals("update.do")) {
+			controller = new UpdateController();
+		} else if(command.equals("allMember.do")) {
+			controller = new AllMemberController();
+		} else if(command.equals("logout.do")) {
+			controller = new LogoutController();
+		}
+		
+		return controller;
+	}
+}
